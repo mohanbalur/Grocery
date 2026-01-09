424e28b069aa289f4829963b47a0072616eed893
Sneha <mohanbalur2004@gmail.com>
2026-01-09 15:31:05 +0530

    first commit

--- grocery-website/frontend/src/api/order.api.js
+++ grocery-website/frontend/src/api/order.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://grocery-llkq.onrender.com/api",
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
 });
 
 /* ✅ Always read token from userInfo */
